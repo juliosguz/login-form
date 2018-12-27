c43918e3b6618a4e2a3724d005d3a387855f230b
Julio Silva <julio.sguz@gmail.com>
2018-12-26 21:14:56 -0400

    Create basic routes for auth module

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/**
+  PATH    RESULT
+  ''  ->  http://localhost:4200
+  'auth'  ->  http://localhost:4200/auth
+  'users'  ->  http://localhost:4200/users
+*/
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
